d8f88b580ff0b4c657ca2590f3194f9e202e9ba2
Anton <antongyrdymov@yandex.ru>
2023-10-15 21:10:33 +0300

    upd after remark

--- 1.py
+++ 1.py
@@ -5,17 +5,16 @@ from tkinter import ttk
 import pygame
 
 
-
 def clicked():
-    ch = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    res = txt.get()
-    ans = res + '-'
-    for x in res:
-        ans += ch[(ch.index(x) + 3) % len(ch)]
-    ans += '-'
-    for x in res:
-        ans += ch[(ch.index(x) - 5) % len(ch)]
-    lbl = Label(tab1, text=f'Ваш ключ: {ans}', font=('Arial Bold', 30), relief="groove")
+    characters = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    input_text = text_enter.get()
+    key = input_text + '-'
+    for ch in input_text:
+        key += characters[(characters.index(ch) + 3) % len(characters)]
+    key += '-'
+    for ch in input_text:
+        key += characters[(characters.index(ch) - 5) % len(characters)]
+    lbl = Label(tab1, text=f'Ваш ключ: {key}', font=('Arial Bold', 30), relief="groove")
     lbl.grid(row=4)
     lbl.place(anchor=CENTER, relx=0.5, rely=0.5)
 
@@ -27,10 +26,10 @@ def play():
 
 window = Tk()
 window.title('Key generator for minecraft :)')
-window.maxsize(1000, 500)
-w = 1600
-h = 1100
-window.geometry(f"{w}x{h}")
+window.maxsize(1600, 1000)
+width = 1000
+hight = 500
+window.geometry(f"{width}x{hight}")
 
 tab_control = ttk.Notebook(window)
 tab1 = ttk.Frame(tab_control)
@@ -39,24 +38,24 @@ tab_control.add(tab1, text='Основное')
 tab_control.add(tab2, text='Доп')  
 tab_control.pack(expand=1, fill='both') 
 
-filename = PhotoImage(file="itmo_study/laba4/imgpreview.png")
-background_label = Label(tab1, image=filename)  
+image_game = PhotoImage(file="itmo_study/laba4/imgpreview.png")
+background_label = Label(tab1, image=image_game)  
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-filename1 = PhotoImage(file="itmo_study/laba4/2731746.jpg.png")
-background_label1 = Label(tab2, image=filename1, border=50)
+image_bg = PhotoImage(file="itmo_study/laba4/2731746.jpg.png")
+background_label1 = Label(tab2, image=image_bg, border=50)
 background_label1.place(x=0, y=0, relwidth=1, relheight=1)
 
 lbl_hi = Label(tab1, text='Введите первую часть вашего ключа!', font=('Arial Bold', 40), relief="groove")
 lbl_hi.grid(column=0, row=0)
 lbl_hi.place(anchor=CENTER, relx=0.5, rely=0.1)
 
-txt = Entry(tab1, width=10, borderwidth=0)
-txt.grid(column=0, row=0)
-txt.place(anchor=CENTER, relx=0.5, rely=0.25)
-txt.focus()
+text_enter = Entry(tab1, width=10, borderwidth=0)
+text_enter.grid(column=0, row=0)
+text_enter.place(anchor=CENTER, relx=0.5, rely=0.25)
+text_enter.focus()
 
-btn = Button(tab1, text='do this', bg='red', fg='black', command=clicked, border=0)
+btn = Button(tab1, text='generate', bg='red', fg='black', command=clicked, border=0)
 btn.grid(column=0)
 btn.place(anchor=CENTER, relx=0.5, rely=0.35)
 
--- 2.py
+++ 2.py
@@ -2,26 +2,26 @@ from tkinter import *
 from tkinter.ttk import *
 
 
-ws = Tk()
-Progress_Bar = Progressbar(ws, orient=HORIZONTAL, length=250, mode='determinate')
+window = Tk()
+Progress_Bar = Progressbar(window, orient=HORIZONTAL, length=250, mode='determinate')
 
 
 def Slide():
     import time
     Progress_Bar['value'] = 20
-    ws.update_idletasks()
+    window.update_idletasks()
     
-    time.sleep(1)
+    time.sleep(0.6)
     Progress_Bar['value'] = 50
-    ws.update_idletasks()
+    window.update_idletasks()
 
-    time.sleep(1)
+    time.sleep(0.6)
     Progress_Bar['value'] = 80
-    ws.update_idletasks() 
+    window.update_idletasks() 
 
-    time.sleep(1)
+    time.sleep(0.6)
     Progress_Bar['value'] = 100
 
 Progress_Bar.pack()
-Button(ws, text='Run', command=Slide).pack(pady=10)
+Button(window, text='Run', command=Slide).pack(pady=10)
 mainloop()
