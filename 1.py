c8c43bb69aee4b02b46c84f45b8c6fa2127b74f4
Anton <antongyrdymov@yandex.ru>
2023-11-15 19:24:08 +0300

    minor update

--- 1.py
+++ 1.py
@@ -6,30 +6,34 @@ import pygame
 
 
 def clicked():
-    characters = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    CHARACTERS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    OFFSET_R = 3
+    OFFSET_L = 5
     input_text = text_enter.get()
     key = input_text + '-'
     for ch in input_text:
-        key += characters[(characters.index(ch) + 3) % len(characters)]
+        key += CHARACTERS[(CHARACTERS.index(ch) + OFFSET_R) % len(CHARACTERS)]
     key += '-'
     for ch in input_text:
-        key += characters[(characters.index(ch) - 5) % len(characters)]
+        key += CHARACTERS[(CHARACTERS.index(ch) - OFFSET_L) % len(CHARACTERS)]
     lbl = Label(tab1, text=f'Ваш ключ: {key}', font=('Arial Bold', 30), relief="groove")
     lbl.grid(row=4)
     lbl.place(anchor=CENTER, relx=0.5, rely=0.5)
 
 
 def play():
-    pygame.mixer.music.load("itmo_study/laba4/Glass.aiff")
+    pygame.mixer.music.load("Glass.aiff")
     pygame.mixer.music.play()
 
 
+M_WIDTH = 1600
+M_HIGHT = 1000
+WIDTH = 1000
+HIGHT = 500
 window = Tk()
 window.title('Key generator for minecraft :)')
-window.maxsize(1600, 1000)
-width = 1000
-hight = 500
-window.geometry(f"{width}x{hight}")
+window.maxsize(M_WIDTH, M_HIGHT)
+window.geometry(f"{WIDTH}x{HIGHT}")
 
 tab_control = ttk.Notebook(window)
 tab1 = ttk.Frame(tab_control)
@@ -38,11 +42,11 @@ tab_control.add(tab1, text='Основное')
 tab_control.add(tab2, text='Доп')  
 tab_control.pack(expand=1, fill='both') 
 
-image_game = PhotoImage(file="itmo_study/laba4/imgpreview.png")
+image_game = PhotoImage(file="imgpreview.png")
 background_label = Label(tab1, image=image_game)  
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-image_bg = PhotoImage(file="itmo_study/laba4/2731746.jpg.png")
+image_bg = PhotoImage(file="2731746.jpg.png")
 background_label1 = Label(tab2, image=image_bg, border=50)
 background_label1.place(x=0, y=0, relwidth=1, relheight=1)
 
--- 2.py
+++ 2.py
@@ -2,24 +2,26 @@ from tkinter import *
 from tkinter.ttk import *
 
 
+LENGTH = 250
 window = Tk()
-Progress_Bar = Progressbar(window, orient=HORIZONTAL, length=250, mode='determinate')
+Progress_Bar = Progressbar(window, orient=HORIZONTAL, length=LENGTH, mode='determinate')
 
 
 def Slide():
+    WAIT_TIME = 0.6
     import time
     Progress_Bar['value'] = 20
     window.update_idletasks()
     
-    time.sleep(0.6)
+    time.sleep(WAIT_TIME)
     Progress_Bar['value'] = 50
     window.update_idletasks()
 
-    time.sleep(0.6)
+    time.sleep(WAIT_TIME)
     Progress_Bar['value'] = 80
     window.update_idletasks() 
 
-    time.sleep(0.6)
+    time.sleep(WAIT_TIME)
     Progress_Bar['value'] = 100
 
 Progress_Bar.pack()
